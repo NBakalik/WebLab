ca273e957c770bdb1b78ab1d72bf1e2f5aec808a
NBakalik <bakalik.nazariy@gmail.com>
2021-12-08 14:23:31 +0200

    added lab11 code

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,35 @@
 import {Button, ButtonWrapper, ItemsWrapper, Label, Wrapper} from "./Cart.styles";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {CartItem} from "../CartItem/CartItem";
+import {useState} from "react";
+import {Checkout} from "../Checkout/Checkout";
 
 export function Cart() {
     const items = useSelector((state) => state.items);
+    const [view, toggle] = useState(false);
 
-    return (
-        <Wrapper>
-            <Label>
-                Shopping cart
-            </Label>
-            <ItemsWrapper>
-                {items.map(item => (
-                    <CartItem key={item.name} name={item.name} price={item.price} peopleAmount={item.peopleAmount}/>))}
-            </ItemsWrapper>
-            <ButtonWrapper>
-                <Button>Back to Catalog</Button>
-                <Button>Continue</Button>
-            </ButtonWrapper>
-        </Wrapper>
-    );
+    function toggleView() {
+        toggle(!view);
+    }
+
+    if (!view) {
+        return (
+            <Wrapper>
+                <Label>
+                    Shopping cart
+                </Label>
+                <ItemsWrapper>
+                    {items.map(item => (
+                        <CartItem key={item.name} name={item.name} price={item.price}
+                                  peopleAmount={item.peopleAmount}/>))}
+                </ItemsWrapper>
+                <ButtonWrapper>
+                    <Button>Back to Catalog</Button>
+                    <Button onClick={toggleView}>Continue</Button>
+                </ButtonWrapper>
+            </Wrapper>
+        );
+    } else {
+        return <Checkout func={toggleView}/>
+    }
 }
